383a2a1253d64e6918a3fec84ae68fbe00d9ddce
igonzalezb <igonzalezb@users.noreply.github.com>
2026-07-22 09:30:36 -0300

    Make semihosting conditional on DEBUG
    Wrap LOGGER_CONFIG_USE_SEMIHOSTING in a conditional compilation block so semihosting is enabled only in DEBUG builds. This disables the overhead of semihosting in release builds.

--- tdse-tf_3-03/app/inc/logger.h
+++ tdse-tf_3-03/app/inc/logger.h
@@ -53,7 +53,12 @@ extern "C" {
 
 #define LOGGER_CONFIG_ENABLE                    (1)
 #define LOGGER_CONFIG_MAXLEN                    (64)
-#define LOGGER_CONFIG_USE_SEMIHOSTING           (1)
+
+#ifdef DEBUG
+    #define LOGGER_CONFIG_USE_SEMIHOSTING (1)
+#else
+    #define LOGGER_CONFIG_USE_SEMIHOSTING (0)
+#endif
 
 #if 1 == LOGGER_CONFIG_ENABLE
 #define LOGGER_LOG(...)\
